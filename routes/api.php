25c4e85566d4d043f7aedef535b29f9f9daf8635
NourKhalil24 <nk146396@gmail.com>
2024-05-05 20:34:36 +0300

    last update

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 Route::controller(AuthController::class)->group(function () {
     Route::post('login', 'login');
-    Route::post('register', 'register');
     Route::post('logout', 'logout')->middleware('auth:api');
     Route::post('refresh', 'refresh')->middleware('auth:api');
 
